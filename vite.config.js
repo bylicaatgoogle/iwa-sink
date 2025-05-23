79e91042b850def43ebcbb856aeb3125db6468ca
Dominik Bylica <bylica@google.com>
2025-05-23 07:56:16 +0000

    Do no fall over when unable to create a signed web bundle.

--- vite.config.js
+++ vite.config.js
@@ -27,33 +27,37 @@ dotenv.config();
 const plugins = [injectHTML()];
 
 if (process.env.NODE_ENV === 'production') {
-  // Get the key and decrypt it to sign the web bundle
-  const key = wbnSign.parsePemKey(
-    process.env.KEY || fs.readFileSync('./certs/encrypted_key.pem'),
-    process.env.KEY_PASSPHRASE ||
-      (await wbnSign.readPassphrase(
-        /*description=*/ './certs/encrypted_key.pem',
-      )),
-  );
+  try {
+    // Get the key and decrypt it to sign the web bundle
+    const key = wbnSign.parsePemKey(
+      process.env.KEY || fs.readFileSync('./certs/encrypted_key.pem'),
+      process.env.KEY_PASSPHRASE ||
+        (await wbnSign.readPassphrase(
+          /*description=*/ './certs/encrypted_key.pem',
+        )),
+    );
 
-  // Add the wbn bundle only during a production build
-  plugins.push({
-    ...wbn({
-      // Ensures the web bundle is signed as an isolated web app
-      baseURL: new wbnSign.WebBundleId(key).serializeWithIsolatedWebAppOrigin(),
-      // Ensure that all content in the `public` directory is included in the web bundle
-      static: {
-        dir: 'public',
-      },
-      // The name of the output web bundle
-      output: 'iwa-sink.swbn',
-      // This ensures the web bundle is signed with the key
-      integrityBlockSign: {
-        strategy: new wbnSign.NodeCryptoSigningStrategy(key),
-      },
-    }),
-    enforce: 'post',
-  });
+    // Add the wbn bundle only during a production build
+    plugins.push({
+      ...wbn({
+        // Ensures the web bundle is signed as an isolated web app
+        baseURL: new wbnSign.WebBundleId(key).serializeWithIsolatedWebAppOrigin(),
+        // Ensure that all content in the `public` directory is included in the web bundle
+        static: {
+          dir: 'public',
+        },
+        // The name of the output web bundle
+        output: 'iwa-sink.swbn',
+        // This ensures the web bundle is signed with the key
+        integrityBlockSign: {
+          strategy: new wbnSign.NodeCryptoSigningStrategy(key),
+        },
+      }),
+      enforce: 'post',
+    });
+  } catch(e) {
+    console.error(e, 'failed trying to create a signed web bundle');
+  }
 }
 
 export default defineConfig({
